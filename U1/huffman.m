b61321ac5b01b6b6b42cb4db99bad9213dc1cfe9
MatyPok <126959319+MatyPok@users.noreply.github.com>
2024-11-18 10:06:22 +0100

    Update huffman.m

--- U1/huffman.m
+++ U1/huffman.m
@@ -1,5 +1,5 @@
 function [Y_encoded, code_table]=huffman(Y)
-% Y_decoded = vektor (double) s vektorem Y přepsaným pomocí Huffmanova
+% Y_decoded = vector (double) s vektorem Y přepsaným pomocí Huffmanova
 % kódování
 % code_table = seřazený vektor hodnot nacházejících se ve vektoru Y a vektor svým pořadím odpovídající V s kódy pro hodnoty z Y
 
@@ -11,12 +11,17 @@ function [Y_encoded, code_table]=huffman(Y)
 
 %Y = [172 126 126 126 126 126 126 126 126 195 195 21 21 21 21];
 
+% nalezení unikátních hodnot v Y
 values = unique(Y);
 %counts = tabulate(Y);
+
+% absolutní četnosti unikátních hodnot
 counts = histcounts(Y, [values, max(values)+1]);
 
 %probs = counts(:,3);
 %values = num2cell(counts(:,1));
+
+% seřazení podle četností
 [probs, idx] = sort(counts);
 hodnoty = values(idx);
 values = num2cell(values(idx));
@@ -25,9 +30,9 @@ values = num2cell(values(idx));
 % codes(:) = {''}; % Inicializace prázdných řetězců pro kódy
 codes = num2cell(repmat("", size(values))); % prázdná buňka pro kódy
 
+% dokud se ve vektoru četností nachází hodnoty
 while length(probs) > 1
-    % create two nodes with the smallest probalities and their summed
-    % probability
+    % vytvoření dvou uzlů o nejmenší čenosti a jejich kombinovaná četnost
     new_probs = probs(1) + probs(2);
     new_value = {[values{1}, values{2}]};
 
@@ -39,14 +44,14 @@ while length(probs) > 1
         codes{2}{i} = strcat('0', codes{2}{i});
     end
 
-    % Actualize list
+    % aktualizace listu
     probs = [new_probs, probs(3:end)];
     values = [new_value, values(3:end)];
 
     % Sloučíme kódy ze dvou listů do jednoho
     codes = [{[codes{1}, codes{2}]}, codes(3:end)];
 
-    % sort in respect to probabilities
+    % opět seřadíme podle četností
     [probs,idx] = sort(probs);
     values = values(idx);
 
@@ -55,10 +60,11 @@ while length(probs) > 1
     codes;
 end
 
-for i = 1:length(Y)
-    for j = 1:length(values{1})
-        if Y(i)==values{1}(j)
-            Y_encoded(i) = codes{1}(j);
+% Přepsaní vektoru Y do Huffmannova kódu
+for i = 1:length(Y) % Prohledávání hodnot Y
+    for j = 1:length(values{1}) % Ke každé hodnotě z vektoru Y prohledáváme odpovídající ve values
+        if Y(i)==values{1}(j) % pokud nalezneme odpovídající hodnotu,
+            Y_encoded(i) = codes{1}(j); % přidělíme jí kód
         end
     end
 end
@@ -66,7 +72,7 @@ end
 %Y_encoded = str2double(Y_encoded);
 V = cell2mat(values);
 %C = str2double(codes{1});
-code_table = [V;codes{1}];
+code_table = [V;codes{1}]; % návrat tabulky s klíčemi, pro následné dekódování
 
 
 end
