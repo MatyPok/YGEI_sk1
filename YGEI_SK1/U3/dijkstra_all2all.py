61eb8e249b6632c37ce3e734cf307b30010a3f1d
GeodetkaTerka <ter.cern@gmail.com>
2025-03-03 22:04:09 +0100

    REORGANIZACE

---
+++ YGEI_SK1/U3/dijkstra_all2all.py
@@ -0,0 +1,93 @@
+
+from math import inf
+from queue import PriorityQueue
+from queue import Empty
+
+
+# From cv 7
+# find path between two poins from given predecessors
+def rPath(u,v,P):
+    path = []
+    # path shortening
+    while v != u and v != (-1):
+        path.append(v)
+        v = P[v]
+    path.append(v)
+    #if v != u:
+        #print('Incorrect path')
+    return path
+
+# compute the total cost of path from the weights of edges of the path
+def sum_of_weights(path, G):
+    w = 0
+    for i in range(len(path)-1):
+        w = w + G[path[i]][path[i+1]]
+    return w
+
+
+def dijkstra(G, start, end):
+    
+    n = len(G)+1 # lenght of graph
+    d = [inf]*n # all nodes to infinity
+    P = [-1]*n # predecessors to -1
+    
+    PQ = PriorityQueue()
+    PQ.put((0, start)) # adding the start vertex into queue
+    d[start] = 0
+    
+    while not PQ.empty():
+        du, u  = PQ.get() # get the first element
+        
+        # iterate through the neighbours, edge relaxation
+        for v, wuv in G[u].items():
+            if d[v] > d[u] + wuv: # if the path through u is better
+                d[v] = d[u] + wuv 
+                P[v] = u # the path is not through v, but u instead
+                PQ.put((d[v], v)) # putting v into the queue
+    
+    path = rPath(start,end,P)
+    return path, d[v]
+
+
+
+# Test Graph
+G = {
+    1 : {2:8, 3:4, 5:2},
+    2 : {1:8, 3:5, 4:2, 7:6, 8:7},
+    3 : {1:4, 2:5, 6:3, 7:4},
+    4 : {2:2, 9:3},
+    5 : {1:2, 6:5},
+    6 : {3:3, 5:5, 7:5, 8:7, 9:10},
+    7 : {2:6, 3:4, 6:5, 8:3},
+    8 : {2:7, 6:7, 7:3, 9:1},
+    9 : {4:3, 6:10, 8:1}
+}
+
+
+'''P, D = dijkstra(G, 1)
+print(P,D)
+
+path = rPath(1,9,P)
+print(path)'''
+
+
+# Import pyplot for plot and our function for Geopackage to graph conversion
+from ygei_lines2graph import lines2graph_gpkg
+from matplotlib import pyplot as plt
+
+# Call the gpkg2graph function, as an argument select one of 3 options of cost metric,
+# and the .gpkg file for conversion, the default file silnice.gpkg
+#G, C = lines2graph_gpkg(type_of_weight='Euclidean distance')
+
+# call the dijkstra algorithm for every pair of nodes
+all_paths = []
+for start, neigbors in G.items():
+    for end, neigbors in G.items():
+        if start != end:
+            path, D = dijkstra(G, start, end)
+            all_paths.append([path,D])
+
+print(all_paths)
+
+
+
---
+++ YGEI_SK1/U3/jarnik.py
@@ -0,0 +1,99 @@
+
+# Graph definition
+G = {
+    1: {2:8, 3:4, 5:2},
+    2: {1:8, 3:5, 4:2, 7:6, 8:7},
+    3: {1:4, 2:5, 6:3, 7:4},
+    4: {2:2, 9:3},
+    5: {1:2, 6:5},
+    6: {3:3, 5:5, 7:5, 8:7, 9:10},
+    7: {2:6, 3:4, 6:5, 8:3},
+    8: {2:7, 6:7, 7:3, 9:1},
+    9: {4:3, 6:10, 8:1},
+}
+
+def jarnik(G, start=1):
+    """
+    Implements Prim's algorithm to find the Minimum Spanning Tree (MST) of a graph.
+
+    
+    Parameters:
+        G: dict
+            Graph represented as an adjacency list.
+        start: int
+            Starting vertex for Prim's algorithm.
+    
+    Returns:
+        T: list
+            List of edges in the MST.
+        total_weight: int
+            Total weight of the MST.
+    """
+    visited = set()  # Set to track visited nodes
+    T = []  # initialize the empty list
+    wt = 0  # initial weight of tree is 0
+
+    # Start with the starting node
+    visited.add(start)
+    while len(visited) < len(G):
+        # Find the smallest edge that connects a visited node to an unvisited node
+        min_edge = None
+        for u in visited:
+            for v, w in G[u].items():
+                if v not in visited:  # Check if the edge leads to an unvisited node
+                    if min_edge is None or w < min_edge[2]:
+                        min_edge = (u, v, w)
+
+    
+        if min_edge:
+            u, v, w = min_edge
+            T.append((u, v, w))
+            wt = wt + w
+            visited.add(v)  # Mark the new node as visited
+
+    
+    return T, wt
+
+
+# Test Prim's algorithm
+T, weight = jarnik(G)
+print("Minimum Spanning Tree:", T)
+print("Total Weight:", weight)
+
+
+# Import pyplot for plot and our function for Geopackage to graph conversion
+from ygei_lines2graph import lines2graph_gpkg
+from matplotlib import pyplot as plt
+
+# Call the gpkg2graph function, as an argument select one of 3 options of cost metric,
+# and the .gpkg file for conversion, the default file silnice.gpkg
+G, C = lines2graph_gpkg(type_of_weight='Euclidean distance')
+
+from obce_load import obcefromgpkg, obec2node_of_graph
+O = obcefromgpkg()
+OC = obec2node_of_graph(O,C)
+
+# Test Prim's algorithm
+#T, weight = jarnik(G)
+#print("Minimum Spanning Tree:", T)
+#print("Total Weight:", weight)
+
+# graphically display the network and the computed path (greed)
+for node, neighbors in G.items():
+    x, y = C[node]
+    for neighbor in neighbors:  
+        nx, ny = C[neighbor] 
+        if node in [t[0] for t in T] and neighbor in [t[1] for t in T]:
+            plt.plot([x, nx], [y, ny], 'g-')
+        else:
+            plt.plot([x, nx], [y, ny], 'c-')
+    
+    plt.plot(x, y, 'ro')
+    #plt.text(x, y, str(node), fontsize=8, ha='right', va='bottom', color='black')
+    try:
+        name = OC[node]
+        plt.text(x, y, str(name), fontsize=8, ha='right', va='bottom', color='black')
+    except:
+        continue
+plt.show()
+
